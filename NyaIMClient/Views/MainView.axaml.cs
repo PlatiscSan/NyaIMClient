ff6d2c79acb3b120e5c5180b09f58805b9000b0a
PlasticSan <lanjack271@gmail.com>
2025-06-10 17:40:10 +0800

    Add project files.

---
+++ NyaIMClient/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NyaIMClient.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ NyaIMClient/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NyaIMClient.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
